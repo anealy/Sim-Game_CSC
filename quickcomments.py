d80aad717a42aa3afd1adb17d991db21954c8dd1
MrMan-ctrl <dconrowxb@gmail.com>
2024-11-19 11:45:07 -0500

    added quickcomments

---
+++ quickcomments.py
@@ -0,0 +1,11 @@
+#########################################
+ #! Example comments
+ #& functions and definitions.
+ #? questions
+ #TODO
+
+ #^ Dylan 
+ #~ Peter
+ #* Aaron
+########################################
+#^ example comments. add a character (specifically space) before the hash#.

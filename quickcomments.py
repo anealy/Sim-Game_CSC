a3b9c10b2daffbcb940f67668897e6b0b871979a
MrMan-ctrl <dconrowxb@gmail.com>
2024-11-19 11:49:51 -0500

    fa

--- quickcomments.py
+++ quickcomments.py
@@ -9,3 +9,12 @@
  #* Aaron
 ########################################
 #^ example comments. add a character (specifically space) before the hash#.
+
+
+#TODO - classes for villans at least 
+#TODO - class for heros. 3x character variables
+#TODO - lore - endgame. die, become lame monster
+#TODO - battle loop
+#TODO - status effects
+#TODO - heal items #potions - eat frog.
+#TODO - 

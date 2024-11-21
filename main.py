94754d4b99bd7eb20595025c1ab15230164b1675
MrMan-ctrl <dconrowxb@gmail.com>
2024-11-21 11:57:34 -0500

    commint

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import random
 
 class Being:
-    def __init__(self):
-        self.hp = 10
-        self.strength = 2
-        self.STR_mod = 0
-
-
-class Monster(Being): x
-    def __init__(self):
-        super().__init__(self,streng):
-        self.hp = 10
-        self.strength = 2
+    def __init__(self,Str_mod):
+        self.hp =  10
+        self.Str = 2
+        self.STR_MOD = Str_mod #default 0
         
 
+
+class Monster(Being): 
+    def __init__(self,Hp, Str, Str_mod):
+        super().__init__(self,Hp,Str,Str_mod)
+        self.str = 
+        
+b = Being()
+b.hp = 
 
 class Hero:
     def __init__(self):
@@ -23,40 +24,40 @@ class Hero:
 
 
 
-def d20():
-    num = random.randrange(1,20)
-    return num
+# def d20():
+#     num = random.randrange(1,20)
+#     return num
 
-def d8():
-    num = random.randrange(1,8)
-    return num
+# def d8():
+#     num = random.randrange(1,8)
+#     return num
 
-def attack(Room()):
-    lroll = d20()
-    if(lroll > 10):
-        damage = d8()
-        print("")
-    if(lroll == 20):
-        damage = 2 * d8()
-    if(lroll <= 10):
-        print(f"")
-
-
-class Room:
-    def __init__(self):
-        self.monster = Monster()
-        self.hero = Hero()
-        self.treasure = "gold coins"
-        self.seen_rooms = "seen_rooms"
-    def battle(self):
-      hroll = d20()
-      vroll = d20()  
-      if(hroll > vroll):
-          self.hero.attack(self)
+# def attack(Room()):
+#     lroll = d20()
+#     if(lroll > 10):
+#         damage = d8()
+#         print("")
+#     if(lroll == 20):
+#         damage = 2 * d8()
+#     if(lroll <= 10):
+#         print(f"")
 
 
+# class Room:
+#     def __init__(self):
+#         self.monster = Monster()
+#         self.hero = Hero()
+#         self.treasure = "gold coins"
+#         self.seen_rooms = "seen_rooms"
+#     def battle(self):
+#       hroll = d20()
+#       vroll = d20()  
+#       if(hroll > vroll):
+#           self.hero.attack(self)
 
 
 
 
-def main():
+
+
+# def main():
